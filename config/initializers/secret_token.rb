4291499bd825a164d67422d2ffdc3134364135d4
alban vervust <albanvervust@MacBook-Pro-de-alban-vervust.local>
2012-12-12 21:31:43 +0100

    premier commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brouillon::Application.config.secret_token = '998af841a5e676863547f4138eace2832071db37912d038ba837a630e5f7e524d84b6efffa9d38362ff074e97f058f57cdc66b5db374e58fb6c443ddd86ded65'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Brouillon::Application.config.session_store :cookie_store, :key => '_brouillon_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Brouillon::Application.config.session_store :active_record_store
